452aa54bca8744ddb567e1be6d2cba8380f9edc9
Anthony Kuske <anthonykuske@gmail.com>
2017-01-25 01:02:26 +0000

    Hide repeat on tennis courts

--- web/areas/tennis_courts.php
+++ web/areas/tennis_courts.php
@@ -1,7 +1,12 @@
 <!-- Display the additional fields for booking tennis courts -->
 
+<!-- Apply additional styles for the tennis courts booking page -->
 <style type="text/css">
-    #mrbs-booking-all-day {
+    #mrbs-booking-all-day,
+    #mrbs-booking-repeat-type,
+    #mrbs-booking-repeat-end-date,
+    #mrbs-booking-repeat-day,
+    #mrbs-booking-repeat-weeks {
         display: none;
     }
 </style>
