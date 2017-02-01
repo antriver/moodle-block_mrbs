85f68f781ec7f11f10a865e75bbe0af1c815395d
Anthony Kuske <anthonykuske@gmail.com>
2017-02-01 02:12:42 +0000

    Add tennis conditions

--- web/areas/tennis_courts.php
+++ web/areas/tennis_courts.php
@@ -1,4 +1,14 @@
 <!-- Display the additional fields for booking tennis courts -->
+<div id="booking-conditions" class="alert alert-info">
+    <h3>Conditions of Use</h3>
+    <ul>
+        <li>If you wish to play at night and require lights please contact Katalyn Hu directly<!-- on (phone number and email)-->.
+            A fee <!--of XX/RMB--> per hour will be charged for use of lights.</li>
+        <li>Please note that school use takes priority over any public booking. In the event the
+            school has to cancel your booking the school will contact you via the mobile number you
+            leave in your booking details.</li>
+    </ul>
+</div>
 
 <!-- Apply additional styles for the tennis courts booking page -->
 <style type="text/css">
